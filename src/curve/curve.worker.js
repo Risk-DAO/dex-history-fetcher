5c2429c652dde839df39aceb80c3d85ceff3364e
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-02-10 09:13:17 +0100

    worker now error logs

--- src/curve/curve.worker.js
+++ src/curve/curve.worker.js
@@ -1,3 +1,4 @@
+const { sleep } = require('../utils/utils');
 const curveConfig = require('./curve.config');
 const { FetchHistory } = require('./curve.history.fetcher');
 
@@ -5,10 +6,24 @@ const { FetchHistory } = require('./curve.history.fetcher');
 
 
 async function main() {
-    for (let i = 0; i < 1; i++) {
-        await FetchHistory(curveConfig.curvePairs[i]);
+    const errors = [];
+
+
+    for (let i = 0; i < curveConfig.curvePairs.length; i++) {
+        try {
+            await FetchHistory(curveConfig.curvePairs[i]);
+        }
+        catch (error) {
+            errors.push(curveConfig.curvePairs[i].poolName);
+            console.log('error fetching pool', curveConfig.curvePairs[i].poolName);
+            console.log('error fetching pool', error);
+        }
+        await sleep(5000);
     }
+    console.log('errorlog', errors);
+
 }
+
 
 
 main();
