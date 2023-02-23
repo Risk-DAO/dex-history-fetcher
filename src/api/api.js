00d9ae5050158051c10daf57a81cc4b137dac8d1
Alfulinku <9356022+alfulinku@users.noreply.github.com>
2023-02-23 15:50:48 +0100

    fix curve available

--- src/api/api.js
+++ src/api/api.js
@@ -41,8 +41,8 @@ function getAvailableCurve() {
                     continue;
                 }
                 
-                available[token][tokenB] = {};
-                available[token][tokenB][poolName] = {};
+                available[token][tokenB] = available[token][tokenB] || {};
+                available[token][tokenB][poolName] = available[token][tokenB][poolName] || {};
                 available[token][tokenB][poolName][token] = reserveValue;
                 available[token][tokenB][poolName][tokenB] = reserveValueB;
             }
