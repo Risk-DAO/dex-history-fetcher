778132b0a17de0a9c5196ac4268f2a4e0088f96e
Alfulinku <Alfulinku@gmail.com>
2023-07-13 16:49:55 +0200

    fetch all time ranges and all slippages when sending to pythia

--- src/pythia/pythia.sender.js
+++ src/pythia/pythia.sender.js
@@ -3,17 +3,17 @@ const BigNumber = require('bignumber.js');
 const pythiaConfig = require('./pythia.config');
 const dotenv = require('dotenv');
 const { fnName, roundTo, sleep, retry } = require('../utils/utils');
-const { getConfTokenBySymbol } = require('../utils/token.utils');
+const { getConfTokenBySymbol, normalize } = require('../utils/token.utils');
 dotenv.config();
 const { getBlocknumberForTimestamp } = require('../utils/web3.utils');
 const { RecordMonitoring } = require('../utils/monitoring');
-const { getAverageLiquidityForBlockInterval, getUniv3PricesForBlockInterval, computeUniv3ParkinsonVolatility } = require('../uniswap.v3/uniswap.v3.utils');
+const { getAverageLiquidityForBlockInterval, computeUniv3ParkinsonVolatility } = require('../uniswap.v3/uniswap.v3.utils');
 const { computeAggregatedVolumeFromPivot } = require('../utils/aggregator');
 
 const CONSTANT_1e18 = new BigNumber(10).pow(18);
 const DATA_DIR = process.cwd() + '/data';
-const TARGET_SLIPPAGE_BPS = 500;
-const daysRange = [30]; //[1, 7, 30, 180, 365];
+const TARGETS_SLIPPAGE_BPS = [100, 500, 1000, 1500, 2000];
+const daysRange = [7, 30, 180, 365];
 const MONITORING_NAME = 'Pythia Sender';
 let slippageCache = {};
 async function SendToPythia() {
@@ -88,33 +88,29 @@ async function SendLiquidityData(daysToAvg, startBlock, endBlock) {
     const pythiaProvider = new ethers.providers.StaticJsonRpcProvider(process.env.PYTHIA_RPC_URL);
     const signer = new ethers.Wallet(process.env.ETH_PRIVATE_KEY, pythiaProvider);
     const pythiaContract = new Contract(pythiaConfig.pythiaAddress, pythiaConfig.pythiaAbi, signer);
-    const keyEncoderContract = new Contract(pythiaConfig.keyEncoderAddress, pythiaConfig.keyEncoderAbi, signer);
 
     const allAssets = [];
     const allKeys = [];
     const allValues = [];
     const allUpdateTimes = [];
 
-    const USDCConf = getConfTokenBySymbol('USDC');
 
     for (const tokenSymbol of pythiaConfig.tokensToPush) {
         // get config 
         const tokenConf = getConfTokenBySymbol(tokenSymbol);
         console.log(`${fnName()}[${tokenSymbol}]: start working on token ${tokenConf.symbol} with address ${tokenConf.address}`);
 
-        console.log(`calling keyEncoderContract.encodeLiquidityKey(${tokenConf.address}, ${USDCConf.address}, ${2}, ${(TARGET_SLIPPAGE_BPS / 100)}, ${daysToAvg})`);
-        const key = await retry(keyEncoderContract.encodeLiquidityKey, [tokenConf.address, USDCConf.address, 2, (TARGET_SLIPPAGE_BPS / 100), daysToAvg]);
-        const dataToSend = await getUniv3Average(tokenConf, daysToAvg, startBlock, endBlock);
+        const dataToSend = await getUniv3Average(tokenConf, daysToAvg, startBlock, endBlock, signer);
 
         // get the key from the key encoder contract
         console.log(`${fnName()}[${tokenSymbol}]: data to send:`, dataToSend);
-        allAssets.push(dataToSend.asset);
-        allKeys.push(key);
-        allValues.push(dataToSend.value);
-        allUpdateTimes.push(dataToSend.updateTime);
+        allAssets.push(...dataToSend.map(_ => _.asset));
+        allKeys.push(...dataToSend.map(_ => _.key));
+        allValues.push(...dataToSend.map(_ => _.value));
+        allUpdateTimes.push(...dataToSend.map(_ => _.updateTime));
     }
 
-    const gas = pythiaConfig.tokensToPush.length * 30000;
+    const gas = allAssets.length * 30000;
     const txResponse = await retry(pythiaContract.multiSet, [allAssets, allKeys, allValues, allUpdateTimes, { gasLimit: gas }]);
 
     let txFinished = false;
@@ -188,45 +184,65 @@ async function SendVolatilityData(daysToAvg, startBlock, endBlock) {
  * @param {number} endBlock 
  * @returns 
  */
-async function getUniv3Average(tokenConf, daysToAvg, startBlock, endBlock) {
-    console.log(`${fnName()}[${tokenConf.symbol}]: start finding data for ${TARGET_SLIPPAGE_BPS}bps slippage since block ${startBlock}`);
-    const avgResult = getCachedAverageLiquidityForBlockInterval(DATA_DIR, tokenConf.symbol, 'USDC',  startBlock, endBlock);
-    let avgLiquidityForTargetSlippage = avgResult.slippageMapAvg[TARGET_SLIPPAGE_BPS];
-    console.log(`${fnName()}[${tokenConf.symbol}]: Computed average liquidity for ${TARGET_SLIPPAGE_BPS}bps slippage: ${avgLiquidityForTargetSlippage}`);
+async function getUniv3Average(tokenConf, daysToAvg, startBlock, endBlock, signer) {
     
-    // add volumes from WBTC and WETH pivots
-    for(const pivot of ['WBTC', 'WETH']) {
-        if(tokenConf.symbol == pivot) {
-            continue;
-        }
+    const USDCConf = getConfTokenBySymbol('USDC');
+    const keyEncoderContract = new Contract(pythiaConfig.keyEncoderAddress, pythiaConfig.keyEncoderAbi, signer);
 
-        const segment1AvgResult = getCachedAverageLiquidityForBlockInterval(DATA_DIR, tokenConf.symbol, pivot,  startBlock, endBlock);
-        if(!segment1AvgResult) {
-            console.log(`Could not find data for ${tokenConf.symbol}->${pivot}`);
-            continue;
+    console.log(`${fnName()}[${tokenConf.symbol}]: start finding data for ${TARGETS_SLIPPAGE_BPS.join(', ')} bps slippage since block ${startBlock}`);
+    const avgResult = getCachedAverageLiquidityForBlockInterval(DATA_DIR, tokenConf.symbol, 'USDC', startBlock, endBlock);
+
+    const results = [];
+
+    for(const TARGET_SLIPPAGE_BPS of TARGETS_SLIPPAGE_BPS) {
+        let avgLiquidityForTargetSlippage = avgResult.slippageMapAvg[TARGET_SLIPPAGE_BPS];
+        console.log(`${fnName()}[${tokenConf.symbol}]: Computed average liquidity for ${TARGET_SLIPPAGE_BPS}bps slippage: ${avgLiquidityForTargetSlippage}`);
+        console.log(`calling keyEncoderContract.encodeLiquidityKey(${tokenConf.address}, ${USDCConf.address}, ${0}, ${(TARGET_SLIPPAGE_BPS / 100)}, ${daysToAvg})`);
+        const key = await retry(keyEncoderContract.encodeLiquidityKey, [tokenConf.address, USDCConf.address, 0, (TARGET_SLIPPAGE_BPS / 100), daysToAvg]);
+
+        // add volumes from WBTC and WETH pivots
+        for(const pivot of ['WBTC', 'WETH']) {
+            if(tokenConf.symbol == pivot) {
+                continue;
+            }
+
+            const segment1AvgResult = getCachedAverageLiquidityForBlockInterval(DATA_DIR, tokenConf.symbol, pivot,  startBlock, endBlock);
+            if(!segment1AvgResult) {
+                console.log(`Could not find data for ${tokenConf.symbol}->${pivot}`);
+                continue;
+            }
+            const segment2AvgResult = getCachedAverageLiquidityForBlockInterval(DATA_DIR, pivot, 'USDC',  startBlock, endBlock);
+            if(!segment2AvgResult) {
+                console.log(`Could not find data for ${pivot}->USDC`);
+                continue;
+            }
+    
+            const aggregVolume = computeAggregatedVolumeFromPivot(segment1AvgResult.slippageMapAvg, segment1AvgResult.averagePrice, segment2AvgResult.slippageMapAvg, TARGET_SLIPPAGE_BPS);
+            console.log(`adding aggreg volume ${aggregVolume} from route ${tokenConf.symbol}->${pivot}->USDC for slippage ${TARGET_SLIPPAGE_BPS} bps`);
+            avgLiquidityForTargetSlippage += aggregVolume;
+            console.log(`new aggreg volume for ${tokenConf.symbol}->USDC: ${avgLiquidityForTargetSlippage} for slippage ${TARGET_SLIPPAGE_BPS} bps`);
+            
         }
-        const segment2AvgResult = getCachedAverageLiquidityForBlockInterval(DATA_DIR, pivot, 'USDC',  startBlock, endBlock);
-        if(!segment2AvgResult) {
-            console.log(`Could not find data for ${pivot}->USDC`);
-            continue;
-        }
-        const aggregVolume = computeAggregatedVolumeFromPivot(segment1AvgResult.slippageMapAvg, segment1AvgResult.averagePrice, segment2AvgResult.slippageMapAvg, TARGET_SLIPPAGE_BPS);
-        console.log(`adding aggreg volume ${aggregVolume} from route ${tokenConf.symbol}->${pivot}->USDC for slippage ${TARGET_SLIPPAGE_BPS} bps`);
-        avgLiquidityForTargetSlippage += aggregVolume;
-        console.log(`new aggreg volume for ${tokenConf.symbol}->USDC: ${avgLiquidityForTargetSlippage} for slippage ${TARGET_SLIPPAGE_BPS} bps`);
+        
+
+        // change the computed avg value to a BigNumber with 18 decimals
+        const liquidityInWei = new BigNumber(avgLiquidityForTargetSlippage).times(CONSTANT_1e18).toFixed(0);
+        console.log(`${fnName()}[${tokenConf.symbol}]: liquidityInWei: ${liquidityInWei} for slippage ${TARGET_SLIPPAGE_BPS} bps`);
+        results.push({
+            key: key,
+            asset: tokenConf.address,
+            value: ethers.BigNumber.from(liquidityInWei),
+            updateTime: Math.round(Date.now()/1000), // timestamp in sec
+        });
     }
 
-    // change the computed avg value to a BigNumber with 18 decimals
-    const liquidityInWei = new BigNumber(avgLiquidityForTargetSlippage).times(CONSTANT_1e18).toFixed(0);
-    console.log(`${fnName()}[${tokenConf.symbol}]: liquidityInWei: ${liquidityInWei}`);
+    // return all the computed value
+    for(let i = 0; i < TARGETS_SLIPPAGE_BPS.length; i++) {
+        const TARGET_SLIPPAGE_BPS = TARGETS_SLIPPAGE_BPS[i];
+        console.log(`${tokenConf.symbol}/USDC liquidity for ${TARGET_SLIPPAGE_BPS/100}% slippage: ${normalize(results[i].value, 18)} ${tokenConf.symbol}`);
+    }
 
-
-    // return the computed value
-    return {
-        asset: tokenConf.address,
-        value: ethers.BigNumber.from(liquidityInWei),
-        updateTime: Math.round(Date.now()/1000), // timestamp in sec
-    };
+    return results;
 }
 
 function getUniv3ParkinsonVolatility(tokenConf, daysToAvg, startBlock, endBlock) {
