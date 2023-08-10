726918719522c2c4faffcbdc2af016f53d3fb76f
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-08-10 17:47:16 +0200

    successful clf computation

--- src/clf/compoundV3/compoundV3Computer.js
+++ src/clf/compoundV3/compoundV3Computer.js
@@ -7,26 +7,45 @@ const { getBlocknumberForTimestamp } = require('../../utils/web3.utils');
 const { computeUniv3ParkinsonVolatility, getAverageLiquidityForBlockInterval } = require('../../uniswap.v3/uniswap.v3.utils');
 const { computeAggregatedVolumeFromPivot } = require('../../utils/aggregator');
 const { normalize, getConfTokenBySymbol } = require('../../utils/token.utils');
-const compoundV3ComputerConfig = require('./compoundV3Computer.config');
+const { compoundV3Pools, cometABI } = require('./compoundV3Computer.config');
 
 const CONSTANT_1e18 = new BigNumber(10).pow(18);
 const DATA_DIR = process.cwd() + '/data';
 const spans = [7, 30, 180];
 
-const compoundV3Asset = {
-    index:2,
-    symbol:'WETH'
-};
 
-async function computeMarketCLF(from, to) {
-    const web3Provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL);
-    const cometContract = new ethers.Contract('0xc3d688B66703497DAA19211EEdff47f25384cdc3', compoundV3ComputerConfig.cometABI, web3Provider);
+async function main() {
+    /// for all pools in compound v3
+    for (const pool of Object.values(compoundV3Pools)) {
+        console.log(`Started work on Compound v3 --- ${pool.baseAsset} --- pool`);
+        const web3Provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL);
+        const cometContract = new ethers.Contract(pool.cometAddress, cometABI, web3Provider);
+        /// for all collaterals in selected pool
+        for (const collateral of Object.values(pool.collateralTokens)) {
+            console.log(`Computing CLFs for ${collateral.symbol}`);
+            const poolCLFs = await computeMarketCLF(web3Provider, cometContract, collateral, pool.baseAsset);
+            console.log('---------------------------');
+            console.log('---------------------------');
+            console.log('poolCLFs', poolCLFs);
+            console.log('---------------------------');
+            console.log('---------------------------');
+
+        }
+    }
+}
+
+
+
+async function computeMarketCLF(web3Provider, cometContract, compoundV3Asset, to) {
+    const from = compoundV3Asset.symbol;
     const assetParameters = await getAssetParameters(cometContract, compoundV3Asset);
     console.log('assetParameters', assetParameters);
 
 
     const endBlock = await retry((() => web3Provider.getBlockNumber()), []);
-    const results = {};
+    const parameters = {};
+
+    ///Get liquidities and volatilities for all spans
     for (const span of spans) {
         // find block for 'daysToAvg' days ago
         const startBlock = await getBlocknumberForTimestamp(Math.round(Date.now() / 1000) - (span * 24 * 60 * 60));
@@ -35,20 +54,48 @@ async function computeMarketCLF(from, to) {
         const computedVolatility = getVolatility(span, from, to, startBlock, endBlock);
         const computedLiquidity = getLiquidity(assetParameters.liquidationBonusBPS, from, to, startBlock, endBlock);
 
-        results[span] = {
+        parameters[span] = {
             volatility: computedVolatility,
             liquidity: computedLiquidity
         };
     }
-    console.log('results', results);
+    console.log('parameters', parameters);
+    /// compute CLFs for all spans and all volatilities
+    const results = {};
+    for(let i = 0; i < spans.length; i++){
+        const volatilitySpan = spans[i];
+        console.log('volatilitySpan', volatilitySpan);
+        results[volatilitySpan] = {};
+        for(let j = 0; j < spans.length; j ++){
+            const liquiditySpan = spans[j];
+            console.log('liquiditySpan', liquiditySpan);
+
+            results[volatilitySpan][liquiditySpan] = findCLFFromParameters(parameters[volatilitySpan].volatility, parameters[liquiditySpan].liquidity, assetParameters.liquidationBonusBPS / 10000, assetParameters.LTV, assetParameters.supplyCap);
+        }
+    }
+    return results;
 }
 
-async function getAssetParameters(cometContract, compoundV3Asset){
+
+function findCLFFromParameters(volatility, liquidity, liquidationBonus, ltv, borrowCap) {
+    console.log('findCLFFromParameters');
+    console.log(volatility, liquidity, liquidationBonus, ltv, borrowCap);
+    ltv = Number(ltv) / 100;
+    const sqrtResult = Math.sqrt(liquidity/borrowCap);
+    const sqrtBySigma = sqrtResult / volatility;
+    const ltvPlusBeta = Number(ltv) + Number(liquidationBonus);
+    const lnLtvPlusBeta = Math.log(ltvPlusBeta);
+    const c = -1 * lnLtvPlusBeta * sqrtBySigma;
+    return c;
+};
+
+async function getAssetParameters(cometContract, compoundV3Asset) {
     const results = await cometContract.getAssetInfo(compoundV3Asset.index);
     const liquidationBonusBPS = Math.round((1 - normalize(results.liquidationFactor, 18)) * 10000);
+    const LTV = normalize(results.liquidateCollateralFactor, 18) * 100;
     const tokenConf = getConfTokenBySymbol(compoundV3Asset.symbol);
-    const supplyCap =  normalize(results.supplyCap, tokenConf.decimals);
-    return {liquidationBonusBPS, supplyCap};
+    const supplyCap = normalize(results.supplyCap, tokenConf.decimals);
+    return { liquidationBonusBPS, supplyCap, LTV };
 
 }
 
@@ -91,5 +138,4 @@ function getLiquidity(liquididationBonus, from, to, startBlock, endBlock) {
     return avgLiquidityForTargetSlippage;
 }
 
-computeMarketCLF('WETH', 'USDC', 500);
-
+main();
