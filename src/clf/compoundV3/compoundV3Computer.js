aa1dfea3cf8ea2dd61cd969d890ba16dff7e1de3
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-08-10 15:10:23 +0200

    getInfo from comet contract + config

--- src/clf/compoundV3/compoundV3Computer.js
+++ src/clf/compoundV3/compoundV3Computer.js
@@ -6,13 +6,25 @@ dotenv.config();
 const { getBlocknumberForTimestamp } = require('../../utils/web3.utils');
 const { computeUniv3ParkinsonVolatility, getAverageLiquidityForBlockInterval } = require('../../uniswap.v3/uniswap.v3.utils');
 const { computeAggregatedVolumeFromPivot } = require('../../utils/aggregator');
+const { normalize, getConfTokenBySymbol } = require('../../utils/token.utils');
+const compoundV3ComputerConfig = require('./compoundV3Computer.config');
 
 const CONSTANT_1e18 = new BigNumber(10).pow(18);
 const DATA_DIR = process.cwd() + '/data';
 const spans = [7, 30, 180];
 
-async function computeMarketCLF(from, to, liquididationBonus) {
+const compoundV3Asset = {
+    index:2,
+    symbol:'WETH'
+};
+
+async function computeMarketCLF(from, to) {
     const web3Provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL);
+    const cometContract = new ethers.Contract('0xc3d688B66703497DAA19211EEdff47f25384cdc3', compoundV3ComputerConfig.cometABI, web3Provider);
+    const assetParameters = await getAssetParameters(cometContract, compoundV3Asset);
+    console.log('assetParameters', assetParameters);
+
+
     const endBlock = await retry((() => web3Provider.getBlockNumber()), []);
     const results = {};
     for (const span of spans) {
@@ -21,7 +33,7 @@ async function computeMarketCLF(from, to, liquididationBonus) {
         console.log(`${fnName()}: Will avg liquidity since block ${startBlock}`);
 
         const computedVolatility = getVolatility(span, from, to, startBlock, endBlock);
-        const computedLiquidity = getLiquidity(liquididationBonus, from, to, startBlock, endBlock);
+        const computedLiquidity = getLiquidity(assetParameters.liquidationBonusBPS, from, to, startBlock, endBlock);
 
         results[span] = {
             volatility: computedVolatility,
@@ -29,6 +41,15 @@ async function computeMarketCLF(from, to, liquididationBonus) {
         };
     }
     console.log('results', results);
+}
+
+async function getAssetParameters(cometContract, compoundV3Asset){
+    const results = await cometContract.getAssetInfo(compoundV3Asset.index);
+    const liquidationBonusBPS = Math.round((1 - normalize(results.liquidationFactor, 18)) * 10000);
+    const tokenConf = getConfTokenBySymbol(compoundV3Asset.symbol);
+    const supplyCap =  normalize(results.supplyCap, tokenConf.decimals);
+    return {liquidationBonusBPS, supplyCap};
+
 }
 
 function getVolatility(span, from, to, startBlock, endBlock) {
