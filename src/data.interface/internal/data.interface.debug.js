89304a9c914efcb881e5b0985e734a80db5125dd
Alfulinku <Alfulinku@gmail.com>
2023-08-18 16:13:52 +0200

    add more debug fct

--- src/data.interface/internal/data.interface.debug.js
+++ src/data.interface/internal/data.interface.debug.js
@@ -5,33 +5,49 @@ const { getBlocknumberForTimestamp } = require('../../utils/web3.utils');
 const { getLiquidity, getVolatility, getAverageLiquidity } = require('../data.interface');
 
 async function testVolatility() {
-    const daysToAvg = 180;
+    const daysToAvg = 365;
     const web3Provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL);
     const endBlock = await web3Provider.getBlockNumber();
     const startBlock = await getBlocknumberForTimestamp(Math.round(Date.now() / 1000) - (daysToAvg * 24 * 60 * 60));
     const base = 'DAI';
     const quote = 'USDC';
-    const allVol = getVolatility(base, quote, startBlock, endBlock, undefined, daysToAvg);
-    const univ2Vol = getVolatility(base, quote, startBlock, endBlock, ['uniswapv2'], daysToAvg);
-    const univ3Vol = getVolatility(base, quote, startBlock, endBlock, ['uniswapv3'], daysToAvg);
-    const curve = getVolatility(base, quote, startBlock, endBlock, ['curve'], daysToAvg);
+    const univ2Vol = getVolatility('uniswapv2', base, quote, startBlock, endBlock, daysToAvg);
+    const univ3Vol = getVolatility('uniswapv3', base, quote, startBlock, endBlock, daysToAvg);
+    const curve = getVolatility('curve', base, quote, startBlock, endBlock, daysToAvg);
 
-    console.log({allVol}, {univ2Vol}, {univ3Vol}, {curve});
+    console.log({univ2Vol}, {univ3Vol}, {curve});
 }
 async function testLiquidity() {
-    const daysToAvg = 30;
+    const daysToAvg = 7;
     const web3Provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL);
     const endBlock = await web3Provider.getBlockNumber();
     const startBlock = await getBlocknumberForTimestamp(Math.round(Date.now() / 1000) - (daysToAvg * 24 * 60 * 60));
-    const base = 'WETH';
+    const base = 'WBTC';
     const quote = 'USDC';
 
-    const stepBlock = Math.round((endBlock-startBlock) / 50);
+    // const stepBlock = Math.round((endBlock-startBlock) / 50);
+    const stepBlock = 50;
 
-    // const liquidity = getLiquidity(base, quote, startBlock, endBlock, platforms, true, stepBlock);
-    const avgLiquidityUniv2 = getAverageLiquidity('uniswapv2', base, quote, startBlock, endBlock, true);
+    // const liquidityUniv2 = getLiquidity('uniswapv2', base, quote, startBlock, endBlock, true, stepBlock);
+    // const liquidityUniv3 = getLiquidity('uniswapv3', base, quote, startBlock, endBlock, true, stepBlock);
+    // const liquidityCurve = getLiquidity('curve', base, quote, startBlock, endBlock, true, stepBlock);
+    // const avgLiquidityUniv2 = getAverageLiquidity('uniswapv2', base, quote, startBlock, endBlock, true);
+    // const avgLiquidityUniv3 = getAverageLiquidity('uniswapv3', base, quote, startBlock, endBlock, true);
+    // const avgLiquidityCurve = getAverageLiquidity('curve', base, quote, startBlock, endBlock, true);
+
     const avgLiquidityUniv3 = getAverageLiquidity('uniswapv3', base, quote, startBlock, endBlock, true);
-    console.log(avgLiquidityUniv2);
+    console.log(avgLiquidityUniv3.avgSlippageMap[100],
+        avgLiquidityUniv3.avgSlippageMap[500],
+        avgLiquidityUniv3.avgSlippageMap[1000],
+        avgLiquidityUniv3.avgSlippageMap[1500],
+        avgLiquidityUniv3.avgSlippageMap[2000]);
+        const avgLiquidityUniv3NoJump = getAverageLiquidity('uniswapv3', base, quote, startBlock, endBlock, false);
+        console.log(avgLiquidityUniv3NoJump.avgSlippageMap[100],
+            avgLiquidityUniv3NoJump.avgSlippageMap[500],
+            avgLiquidityUniv3NoJump.avgSlippageMap[1000],
+            avgLiquidityUniv3NoJump.avgSlippageMap[1500],
+            avgLiquidityUniv3NoJump.avgSlippageMap[2000]);
+    console.log('debug ended');
 }
 
 // testVolatility();
