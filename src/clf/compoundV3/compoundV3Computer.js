5fb92fdf309ab77c4ad2e0d914f3a85f56e66ee4
Alfulinku <Alfulinku@gmail.com>
2023-08-28 16:03:05 +0200

    various fixes for clf

--- src/clf/compoundV3/compoundV3Computer.js
+++ src/clf/compoundV3/compoundV3Computer.js
@@ -1,7 +1,7 @@
 const { ethers } = require('ethers');
 const dotenv = require('dotenv');
 const path = require('path');
-const { fnName, retry, getDay } = require('../../utils/utils');
+const { fnName, getDay } = require('../../utils/utils');
 const fs = require('fs');
 const { default: axios } = require('axios');
 dotenv.config();
@@ -13,6 +13,12 @@ const { DATA_DIR, PLATFORMS } = require('../../utils/constants');
 const { getVolatility, getAverageLiquidity } = require('../../data.interface/data.interface');
 const spans = [7, 30, 180];
 
+compoundV3Computer(60*60);
+
+/**
+ * Compute the CLFs values for compound v3
+ * @param {number} fetchEveryMinutes 
+ */
 async function compoundV3Computer(fetchEveryMinutes) {
     const MONITORING_NAME = 'CompoundV3 CLF Computer';
     const start = Date.now();
@@ -34,11 +40,12 @@ async function compoundV3Computer(fetchEveryMinutes) {
 
         console.log(`${fnName()}: starting`);
         const web3Provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL);
+        const dateNow = Date.now();
         const currentBlock = await web3Provider.getBlockNumber() - 10;
         const results = {};
         /// for all pools in compound v3
         for (const pool of Object.values(compoundV3Pools)) {
-            results[pool.baseAsset] = await computeCLFForPool(pool);
+            results[pool.baseAsset] = await computeCLFForPool(pool.cometAddress, pool.baseAsset, Object.values(pool.collateralTokens), web3Provider, dateNow, currentBlock);
             const poolData = computeAverageCLFForPool(results[pool.baseAsset]);
             results[pool.baseAsset]['weightedCLF'] = poolData['weightedCLF'];
             results[pool.baseAsset]['totalCollateral'] = poolData['totalCollateral'];
@@ -57,8 +64,6 @@ async function compoundV3Computer(fetchEveryMinutes) {
             weightedCLF: protocolWeightedCLF,
             results
         };
-
-
 
         console.log('firing record function');
         recordResults(toRecord);
@@ -82,78 +87,35 @@ async function compoundV3Computer(fetchEveryMinutes) {
             'error': errorMsg
         });
     }
-
 }
 
-function recordResults(results) {
-    const date = getDay();
-    if (!fs.existsSync(`${DATA_DIR}/clf/${date}`)) {
-        fs.mkdirSync(`${DATA_DIR}/clf/${date}`);
-    }
-    if (!fs.existsSync(`${DATA_DIR}/clf/latest`)) {
-        fs.mkdirSync(`${DATA_DIR}/clf/latest`);
-    }
-    const datedProtocolFilename = path.join(DATA_DIR, `clf/${date}/${date}_compoundv3_CLFs.json`);
-    const latestFullFilename = path.join(DATA_DIR, 'clf/latest/compoundv3_CLFs.json');
-    const objectToWrite = JSON.stringify(results);
-    console.log('recording results');
-    try {
-        fs.writeFileSync(datedProtocolFilename, objectToWrite, 'utf8');
-        fs.writeFileSync(latestFullFilename, objectToWrite, 'utf8');
-    }
-    catch (error) {
-        console.log(error);
-        console.log('Compound Computer failed to write files');
-    }
-}
-
-function computeProtocolWeightedCLF(protocolData) {
-    let protocolCollateral = 0;
-    const weightMap = {};
-    for (const marketData of Object.values(protocolData)) {
-        if (marketData) {
-            protocolCollateral += marketData['totalCollateral'];
-        }
-    }
-    // get each collateral weight
-    for (const [market, marketData] of Object.entries(protocolData)) {
-        if (marketData) {
-            const weight = marketData['totalCollateral'] / protocolCollateral;
-            const clf = marketData['weightedCLF'];
-            weightMap[market] = weight * clf;
-        }
-    }
-    let weightedCLF = 0;
-    for (const value of Object.values(weightMap)) {
-        weightedCLF += value;
-    }
-    weightedCLF = (weightedCLF).toFixed(2);
-    return weightedCLF;
-}
-
-async function computeCLFForPool(pool) {
+/**
+ * Compute CLF value for a pool
+ * @param {string} cometAddress 
+ * @param {string} baseAsset 
+ * @param {{index: number, symbol: string, address: string, coingeckoId: string}[]} collaterals 
+ * @param {*} web3Provider 
+ * @param {*} dateNow 
+ * @param {*} endBlock 
+ * @returns 
+ */
+async function computeCLFForPool(cometAddress, baseAsset, collaterals, web3Provider, dateNow, endBlock) {
     const resultsData = {};
-    resultsData['data'] = {};
 
-    console.log(`Started work on Compound v3 --- ${pool.baseAsset} --- pool`);
-    const web3Provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL);
-    const cometContract = new ethers.Contract(pool.cometAddress, cometABI, web3Provider);
+    console.log(`Started work on Compound v3 --- ${baseAsset} --- pool`);
+    const cometContract = new ethers.Contract(cometAddress, cometABI, web3Provider);
     /// for all collaterals in selected pool
-    for (const collateral of Object.values(pool.collateralTokens)) {
+    for (const collateral of collaterals) {
         try {
             console.log(`Computing CLFs for ${collateral.symbol}`);
-            resultsData['data'][collateral.symbol] = {};
-            resultsData['data'][collateral.symbol]['collateral'] = await getCollateralAmount(collateral, cometContract);
-            resultsData['data'][collateral.symbol]['clfs'] = await computeMarketCLF(web3Provider, cometContract, collateral, pool.baseAsset);
-            console.log('---------------------------');
-            console.log('---------------------------');
+            resultsData[collateral.symbol] = {};
+            resultsData[collateral.symbol]['collateral'] = await getCollateralAmount(collateral, cometContract);
+            resultsData[collateral.symbol]['clfs'] = await computeMarketCLF(cometContract, collateral, baseAsset, dateNow, endBlock);
             console.log('resultsData', resultsData);
-            console.log('---------------------------');
-            console.log('---------------------------');
         }
         catch (error) {
             console.log('error', error);
-            resultsData['data'][collateral.symbol] = null;
+            resultsData[collateral.symbol] = null;
         }
     }
     return resultsData;
@@ -177,46 +139,17 @@ async function getCollateralAmount(collateral, cometContract) {
     return results;
 }
 
-function computeAverageCLFForPool(poolData) {
-    //get pool total collateral in usd
-    let totalCollateral = 0;
-    for (const value of Object.values(poolData['data'])) {
-        if (value) {
-            totalCollateral += value['collateral']['usdSupply'];
-        }
-    }
-    const weightMap = {};
-    // get each collateral weight
-    for (const [collateral, value] of Object.entries(poolData['data'])) {
-        if (value) {
-            const weight = value['collateral']['usdSupply'] / totalCollateral;
-            const clf = value['clfs']['7']['7'] ? value['clfs']['7']['7'] : value['clfs']['30']['7'];
-            weightMap[collateral] = weight * clf;
-        }
-    }
-    let weightedCLF = 0;
-    for (const weight of Object.values(weightMap)) {
-        weightedCLF += weight;
-    }
-    weightedCLF = (weightedCLF * 100).toFixed(2);
-    return { weightedCLF, totalCollateral };
-}
-
-
-
-async function computeMarketCLF(web3Provider, cometContract, compoundV3Asset, to) {
+async function computeMarketCLF(cometContract, compoundV3Asset, to, dateNow, endBlock) {
     const from = compoundV3Asset.symbol;
     const assetParameters = await getAssetParameters(cometContract, compoundV3Asset);
     console.log('assetParameters', assetParameters);
 
-
-    const endBlock = await retry((() => web3Provider.getBlockNumber()), []);
     const parameters = {};
 
     ///Get liquidities and volatilities for all spans
     for (const span of spans) {
         // find block for 'daysToAvg' days ago
-        const startBlock = await getBlocknumberForTimestamp(Math.round(Date.now() / 1000) - (span * 24 * 60 * 60));
+        const startBlock = await getBlocknumberForTimestamp(Math.round(dateNow / 1000) - (span * 24 * 60 * 60));
         console.log(`${fnName()}: Will avg liquidity since block ${startBlock}`);
 
         let avgVolatilityAcrossPlatforms = 0;
@@ -248,6 +181,7 @@ async function computeMarketCLF(web3Provider, cometContract, compoundV3Asset, to
             liquidity: sumLiquidityAcrossPlatforms
         };
     }
+
     console.log('parameters', parameters);
     /// compute CLFs for all spans and all volatilities
     const results = {};
@@ -257,23 +191,17 @@ async function computeMarketCLF(web3Provider, cometContract, compoundV3Asset, to
         for (let j = 0; j < spans.length; j++) {
             const liquiditySpan = spans[j];
             if (parameters[volatilitySpan].volatility !== 0) {
-                results[volatilitySpan][liquiditySpan] = findCLFFromParameters(parameters[volatilitySpan].volatility, parameters[liquiditySpan].liquidity, assetParameters.liquidationBonusBPS / 10000, assetParameters.LTV, assetParameters.supplyCap);
+                let volatilityToUse = parameters[volatilitySpan].volatility;
+                if(volatilityToUse < 1 / 10000) {
+                    volatilityToUse = parameters[spans[i+1]].volatility;
+                }
+
+                results[volatilitySpan][liquiditySpan] = findCLFFromParameters(volatilityToUse, parameters[liquiditySpan].liquidity, assetParameters.liquidationBonusBPS / 10000, assetParameters.LTV, assetParameters.supplyCap);
             }
         }
     }
     console.log('results', results);
     return results;
-}
-
-
-function findCLFFromParameters(volatility, liquidity, liquidationBonus, ltv, borrowCap) {
-    ltv = Number(ltv) / 100;
-    const sqrtResult = Math.sqrt(liquidity / borrowCap);
-    const sqrtBySigma = sqrtResult / volatility;
-    const ltvPlusBeta = Number(ltv) + Number(liquidationBonus);
-    const lnLtvPlusBeta = Math.log(ltvPlusBeta);
-    const c = -1 * lnLtvPlusBeta * sqrtBySigma;
-    return c;
 }
 
 async function getAssetParameters(cometContract, compoundV3Asset) {
@@ -286,4 +214,86 @@ async function getAssetParameters(cometContract, compoundV3Asset) {
 
 }
 
+function findCLFFromParameters(volatility, liquidity, liquidationBonus, ltv, borrowCap) {
+    ltv = Number(ltv) / 100;
+    const sqrtResult = Math.sqrt(liquidity / borrowCap);
+    const sqrtBySigma = sqrtResult / volatility;
+    const ltvPlusBeta = Number(ltv) + Number(liquidationBonus);
+    const lnLtvPlusBeta = Math.log(ltvPlusBeta);
+    const c = -1 * lnLtvPlusBeta * sqrtBySigma;
+    return c;
+}
+
+function computeAverageCLFForPool(poolData) {
+    //get pool total collateral in usd
+    let totalCollateral = 0;
+    for (const value of Object.values(poolData)) {
+        if (value) {
+            totalCollateral += value['collateral']['usdSupply'];
+        }
+    }
+    const weightMap = {};
+    // get each collateral weight
+    for (const [collateral, value] of Object.entries(poolData)) {
+        if (value) {
+            const weight = value['collateral']['usdSupply'] / totalCollateral;
+            const clf = value['clfs']['7']['7'] ? value['clfs']['7']['7'] : value['clfs']['30']['7'];
+            weightMap[collateral] = weight * clf;
+        }
+    }
+    let weightedCLF = 0;
+    for (const weight of Object.values(weightMap)) {
+        weightedCLF += weight;
+    }
+    weightedCLF = (weightedCLF * 100).toFixed(2);
+    return { weightedCLF, totalCollateral };
+}
+
+function computeProtocolWeightedCLF(protocolData) {
+    let protocolCollateral = 0;
+    const weightMap = {};
+    for (const marketData of Object.values(protocolData)) {
+        if (marketData) {
+            protocolCollateral += marketData['totalCollateral'];
+        }
+    }
+    // get each collateral weight
+    for (const [market, marketData] of Object.entries(protocolData)) {
+        if (marketData) {
+            const weight = marketData['totalCollateral'] / protocolCollateral;
+            const clf = marketData['weightedCLF'];
+            weightMap[market] = weight * clf;
+        }
+    }
+    let weightedCLF = 0;
+    for (const value of Object.values(weightMap)) {
+        weightedCLF += value;
+    }
+    weightedCLF = (weightedCLF).toFixed(2);
+    return weightedCLF;
+}
+
+function recordResults(results) {
+    const date = getDay();
+    if (!fs.existsSync(`${DATA_DIR}/clf/${date}`)) {
+        fs.mkdirSync(`${DATA_DIR}/clf/${date}`);
+    }
+    if (!fs.existsSync(`${DATA_DIR}/clf/latest`)) {
+        fs.mkdirSync(`${DATA_DIR}/clf/latest`);
+    }
+    const datedProtocolFilename = path.join(DATA_DIR, `clf/${date}/${date}_compoundv3_CLFs.json`);
+    const latestFullFilename = path.join(DATA_DIR, 'clf/latest/compoundv3_CLFs.json');
+    const objectToWrite = JSON.stringify(results);
+    console.log('recording results');
+    try {
+        fs.writeFileSync(datedProtocolFilename, objectToWrite, 'utf8');
+        fs.writeFileSync(latestFullFilename, objectToWrite, 'utf8');
+    }
+    catch (error) {
+        console.log(error);
+        console.log('Compound Computer failed to write files');
+    }
+}
+
 module.exports = { compoundV3Computer };
+
