2829c1ae75e652dc9ba273427963043440e50bfa
Alfulinku <Alfulinku@gmail.com>
2023-09-29 09:38:23 +0200

    clean curve multithread

--- scripts/simpleCurve.js
+++
@@ -1,8 +0,0 @@
-const { createUnifiedFileForPair } = require('../src/curve/curve.unified.generator');
-
-async function simpleCurve() {
-    // await createUnifiedFileForPair(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
-    await createUnifiedFileForPair(19_000_000, 'WBTC', 'USDC', 'tricryptoUSDCPool');
-}
-
-simpleCurve();
