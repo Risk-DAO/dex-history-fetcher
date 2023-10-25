22693f22aeecd4346dde9ac136807e53dc28a943
Alfulinku <Alfulinku@gmail.com>
2023-10-25 17:06:07 +0200

    cleanups

--- src/data.interface/internal/data.interface.price.js
+++ src/data.interface/internal/data.interface.price.js
@@ -1,35 +1,11 @@
 // price related functions
 
 const { DEFAULT_STEP_BLOCK, PLATFORMS } = require('../../utils/constants');
-const { arrayAverage, fnName, roundTo } = require('../../utils/utils');
+const { fnName, roundTo } = require('../../utils/utils');
 const { computeParkinsonVolatility } = require('../../utils/volatility');
 const { getLiquidityForPlatforms } = require('./data.interface.liquidity');
 const { getUnifiedDataForInterval, getPricesAtBlockForInterval } = require('./data.interface.utils');
 
-/**
- * Compute the average price from each platform then re-average for all platforms
- * Example: compute average price of WETH/USDC for univ3 and univ2 separately 
- * then compute (average_univ3 + average_univ2) / 2
- * @param {string} fromSymbol base symbol (WETH, USDC...)
- * @param {string} toSymbol quote symbol (WETH, USDC...)
- * @param {number} fromBlock start block of the query (included)
- * @param {number} toBlock endblock of the query (included)
- * @param {string} platform platform (univ2, univ3...)
- * @returns {number} the average price
- */
-function getAveragePriceForInterval(fromSymbol, toSymbol, fromBlock, toBlock, platform) {
-    const data = getUnifiedDataForInterval(platform, fromSymbol, toSymbol, fromBlock, toBlock);
-    if(!data) {
-        return undefined;
-    }
-    const priceArray = [];
-    for(const dataForBlock of Object.values(data)) {
-        priceArray.push(dataForBlock.price);
-    }
-
-    const avgPrice = arrayAverage(priceArray);
-    return avgPrice;
-}
 
 /**
  * Compute the parkinson's volatility for a pair and a platform
@@ -170,4 +146,4 @@ function getParkinsonVolatilityForIntervalAllPlatformsViaPivot(fromSymbol, toSym
     return volatility;
 }
 
-module.exports = { getAveragePriceForInterval, getParkinsonVolatilityForInterval, getParkinsonVolatilityForIntervalViaPivot, getParkinsonVolatilityForIntervalAllPlatforms, getParkinsonVolatilityForIntervalAllPlatformsViaPivot };
+module.exports = { getParkinsonVolatilityForInterval, getParkinsonVolatilityForIntervalViaPivot, getParkinsonVolatilityForIntervalAllPlatforms, getParkinsonVolatilityForIntervalAllPlatformsViaPivot };
