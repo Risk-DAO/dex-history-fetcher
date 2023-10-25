80cb6183459abc694a6622ea5bec307551ebb6d9
Alfulinku <Alfulinku@gmail.com>
2023-10-25 11:15:24 +0200

    Create compoundV3ComputerDebug.js

---
+++ src/clf/compoundV3/compoundV3ComputerDebug.js
@@ -0,0 +1,17 @@
+const { compoundV3Computer } = require('./compoundV3Computer');
+
+async function compoundV3ComputerDebug() {
+    const startDate = new Date(2023, 5, 11, 12, 0, 0);
+
+    try {
+        await compoundV3Computer(0, startDate.getTime());
+    } catch(e) {
+        console.error(e);
+        process.exitCode = 1;
+    } finally {
+        process.exit();
+    }
+    
+}
+
+compoundV3ComputerDebug();
