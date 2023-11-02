aea4d1f744cabefaacc7294c94c845e76cd8fc61
Alfulinku <Alfulinku@gmail.com>
2023-11-02 10:40:58 +0100

    Update compoundV3ComputerDebug.js

--- src/clf/compoundV3/compoundV3ComputerDebug.js
+++ src/clf/compoundV3/compoundV3ComputerDebug.js
@@ -2,10 +2,12 @@ const { compoundV3Computer } = require('./compoundV3Computer');
 
 async function compoundV3ComputerDebug() {
     const startDates = [];
-    startDates.push(new Date(2023, 5, 14, 12, 43, 15));
-    startDates.push(new Date(2023, 5, 15, 12, 43, 15));
-    // const startDate = new Date(2023, 7, 22, 12, 43, 15);
-    // const startDate = new Date(2023, 7, 23, 12, 43, 15);
+    // startDates.push(new Date(2023, 5, 14, 12, 43, 15));
+    // startDates.push(new Date(2023, 5, 15, 12, 43, 15));
+    // startDates.push(new Date(2023, 6, 31, 12, 43, 15));
+    // startDates.push(new Date(2023, 7, 1, 12, 43, 15));
+    startDates.push(new Date(2023, 7, 22, 12, 43, 15));
+    startDates.push(new Date(2023, 7, 23, 12, 43, 15));
 
     try {
 
